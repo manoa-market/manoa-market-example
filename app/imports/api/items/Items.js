2e1808ce090b3ce76d61d7d5b40026d0cdce823b
Daniel Luong <dnlluong@gmail.com>
2022-11-11 04:35:56 -1000

    Items Collection and Added not tested

---
+++ app/imports/api/items/Items.js
@@ -0,0 +1,29 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Encapsulates state and variable values for this collection. */
+class ItemsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ItemsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      itemName: { type: String, optional: false },
+      image: { type: String, optional: false },
+      condition: { type: String, optional: false },
+      description: { type: String, optional: false },
+      price: { type: String, optional: false },
+      owner: { type: String, unique: false },
+    }, { tracker: Tracker });
+    // Ensure collection documents obey schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const Items = new ItemsCollection();
